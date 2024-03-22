e37814e5595ca0e8cc2d29fda9b07a236697134b
kimzz14 <kimzz1004@gmail.com>
2024-03-22 01:28:26 +0000

    init

---
+++ Syncer.py
@@ -0,0 +1,14 @@
+from lib.Process.Process import PROC
+from datetime import datetime
+import time
+
+while True:
+    now = datetime.now()
+    fout = open('test.txt', 'w')
+    fout.write(now.strftime('%Y-%m-%d %H:%M:%S') + '\n')
+    fout.close()
+
+    process = PROC('rsync -a ')
+    process.run().wait()
+
+    time.sleep(3600)
